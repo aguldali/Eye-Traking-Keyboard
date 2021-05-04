a22bd3259bf61502192e6fef5764fe51fd7fbefe
aguldali <aguldali@havelsan.com.tr>
2021-05-04 15:33:19 +0300

    First Commit
    First Commit

---
+++ Runtime/EyeTrackingDwellObject.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using Microsoft.MixedReality.Toolkit.Input;
+
+[RequireComponent(typeof(EyeTrackingTarget))]
+public class EyeTrackingDwellObject : MonoBehaviour
+{
+    EyeTrackingTarget _eyeTrackingTarget;
+    
+    [Range(0, 10)]
+    public float dwellTimer;
+
+    float timerReset;
+
+    [SerializeField]
+    bool dwellCompleted = false;
+
+    [SerializeField]
+    Image dwellImage;
+
+    [SerializeField]
+    [FormerlySerializedAs("OnDwellEndsAction")]
+    private UnityEvent onDwellEndsAction = null;
+
+    public UnityEvent OnDwellEndsAction
+    {
+        get { return onDwellEndsAction; }
+        set { onDwellEndsAction = value; }
+    }
+
+    private void Start()
+    {
+        _eyeTrackingTarget = GetComponent<EyeTrackingTarget>();
+        timerReset = dwellTimer;
+    }
+
+    private void Update()
+    {
+        if (_eyeTrackingTarget.IsDwelledOn == true && dwellCompleted == false)
+        {
+            dwellTimer -= Time.deltaTime;
+            dwellImage.fillAmount += Time.deltaTime * (1 / timerReset);
+            if(dwellTimer <= 0f)
+            {
+                dwellCompleted = true;
+                dwellImage.fillAmount = 0;
+                OnDwellEndsAction.Invoke();
+            }
+        }
+        else if(_eyeTrackingTarget.IsDwelledOn == false)
+        {
+            dwellCompleted = false;
+            dwellTimer = timerReset;
+            dwellImage.fillAmount = 0;
+        }
+    }
+
+
+}
---
+++ Runtime/EyeTrackingKeyboard.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class EyeTrackingKeyboard : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI targetText;
+
+    [SerializeField]
+    GameObject lowerCase, upperCase, numbers;
+
+
+    bool keyboardShifted = false;
+
+    public void WriteOnTargetText(string newChar)
+    {
+        targetText.text += newChar;
+    }
+    public void DeleteOnTargetText()
+    {
+        if(targetText.text != "")
+         targetText.text = targetText.text.Remove(targetText.text.Length - 1);
+    }
+
+    public void ShiftButtonAction()
+    {
+        if(keyboardShifted == false)
+        {
+            lowerCase.SetActive(false);
+            upperCase.SetActive(true);
+            keyboardShifted = true;
+        }
+        else if(keyboardShifted == true)
+        {
+            lowerCase.SetActive(true);
+            upperCase.SetActive(false);
+            keyboardShifted = false;
+        }
+    }
+
+    public void SpaceButtonAction()
+    {
+        targetText.text += " ";
+    }
+
+}
